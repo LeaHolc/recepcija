470831f91626edabf7b00f214cbb19145f68c158
Lea Holc <holc.lea@gmail.com>
2021-08-24 20:56:27 +0200

    uredila preglednost, jasna imena spremenljivk

--- model.py
+++ model.py
@@ -1,5 +1,4 @@
 import json, os, datetime as dt
-from typing import List, Dict,Optional
 
 DATOTEKA_S_STANJEM="stanje.json"
 
@@ -7,35 +6,31 @@ NOCNINA_ZA_ODRASLE = 14.50
 NOCNINA_ZA_OTROKE_OD_2_DO_14_LET = 7.25
 NOCNINA_ZA_OTROKE_DO_2_LET = 0.00
 
-
-
 TURISTICNA_TAKSA_ODRASLI = 2.00
 TURISTICNA_TAKSA_OTROCI = 1.00
 
-
 class Nocitev:
-    def __init__(self, naziv:str, cena:float, datum):
+
+    def __init__(self, naziv, cena, datum):
         self.naziv = naziv
         self.cena = cena
         self.datum = datum
     
     def v_slovar(self):
-        d = {
+        return {
             "naziv":self.naziv,
             "cena":self.cena,
             "datum":self.datum.isoformat()
         }
-        return d
 
     @staticmethod
     def iz_slovarja(slovar):
         datum = dt.datetime.fromisoformat(slovar['datum']).date()
         return Nocitev(slovar["naziv"], slovar['cena'], datum)
 
-
 class Gost:
 
-    def __init__(self, ime:str, priimek:str, emso:str, drzava:str, nocitve=[]):
+    def __init__(self, ime, priimek, emso, drzava, nocitve=[]):
         self.ime = ime
         self.priimek = priimek
         self.emso = emso
@@ -89,20 +84,18 @@ class Gost:
 
 
     def v_slovar(self):
-        d ={
+        return {
             "ime":self.ime,
             "priimek":self.priimek,
             "emso":self.emso,
             "drzava":self.drzava,
-            "nocitve": [s.v_slovar() for s in self.nocitve]
+            "nocitve": [nocitev.v_slovar() for nocitev in self.nocitve]
         }
-        return d
     
     @staticmethod
     def iz_slovarja(slovar):
-        nocitve = [Nocitev.iz_slovarja( s) for s in slovar.get("nocitve",[])]
-        return Gost(slovar["ime"], slovar["priimek"] ,slovar['emso'], slovar["drzava"], nocitve)
-
+        nocitve = [Nocitev.iz_slovarja(nocitev) for nocitev in slovar.get("nocitve", [])]
+        return Gost(slovar["ime"], slovar["priimek"], slovar['emso'], slovar["drzava"], nocitve)
 
 class Parcela:
     def __init__(self, id_parcele, ime=None):
@@ -113,11 +106,10 @@ class Parcela:
             self.ime = ime 
 
     def v_slovar(self):
-        d = {
+        return {
             "id_parcele":self.id_parcele,
             "ime":self.ime
         }
-        return d
 
     @staticmethod
     def iz_slovarja(slovar):
@@ -129,73 +121,77 @@ class Rezervacija:
         self.id_parcele = str(id_parcele)
         self.gostje = gostje
 
-    def __eq__(self, value:object):
-        if not isinstance(value,Rezervacija): return False
-        return self.id_rezervacije == value.id_rezervacije
+    def __eq__(self, podatki):
+        "preveri, ali podatki določajo rezervacijo"
+        if not isinstance(podatki, Rezervacija): 
+            return False
+        else:
+            return self.id_rezervacije == podatki.id_rezervacije
 
     def v_slovar(self):
-        d = {
+        return {
             "id_rezervacije" : self.id_rezervacije,
             "id_parcele" : self.id_parcele,
-            "gostje": [g.v_slovar() for g in self.gostje]
+            "gostje": [gost.v_slovar() for gost in self.gostje]
         }
-        return d
     
     @staticmethod
-    def iz_slovarja(slovar:dict):
-        gostje = [Gost.iz_slovarja(s) for s in slovar.get("gostje",[])]
+    def iz_slovarja(slovar):
+        gostje = [Gost.iz_slovarja(gost) for gost in slovar.get("gostje", [])]
         return Rezervacija(slovar["id_rezervacije"], slovar["id_parcele"], gostje)
 
 class Model:
-    def __init__(self,datoteka,parcele:Dict[str,Parcela]={},rezervacije:Dict[str,Rezervacija]={}):
+    def __init__(self, datoteka, parcele={}, rezervacije={}):
         self.datoteka = datoteka
         self.parcele = parcele
         self.rezervacije = rezervacije
 
-    def vstavi_rezervacijo(self,r:Rezervacija):
-        kljuc = str(r.id_rezervacije)
+    def vstavi_rezervacijo(self, rezervacija):
+        kljuc = str(rezervacija.id_rezervacije)
         if kljuc in self.rezervacije:
             return None
-        self.rezervacije[kljuc] = r
-        return r
+        self.rezervacije[kljuc] = rezervacija
+        return rezervacija
 
     def dobi_rezervacije(self):
         return self.rezervacije.values()
 
-    def vstavi_parcelo(self,p:Parcela):
-        kljuc = str(p.id_parcele)
+    def vstavi_parcelo(self, parcela):
+        kljuc = str(parcela.id_parcele)
         if kljuc in self.parcele:
             return None
-        self.parcele[kljuc] = p
-        return p
+        self.parcele[kljuc] = parcela
+        return parcela
 
     def dobi_parcele(self):
         return self.parcele
     
     @staticmethod
-    def iz_datoteke(dat:str):
+    def iz_datoteke(datoteka):
 
         ## Če datoteka še ne obstaja
-        if not os.path.exists(dat):
-            m= Model(dat)
+        if not os.path.exists(datoteka):
+            m = Model(datoteka)
             m.shrani()
             return m
+
         ## Preberemo datoteko
-        with open(dat,encoding='utf-8') as json_file:
+        with open(datoteka, encoding='utf-8') as dat:
             try:
-                data:dict = json.load(json_file)
+                data = json.load(dat)
             except json.JSONDecodeError:
                 print("Napaka pri branju JSON datoteke")
-                return Model(dat)
-        parcele = {k:Parcela.iz_slovarja(v) for k, v in data.get("parcele", {}).items()}
-        rezervacije = {k:Rezervacija.iz_slovarja(v) for k, v in data.get("rezervacije", {}).items()}
+                return Model(datoteka)
+
+        parcele = {stevilka:Parcela.iz_slovarja(lastnosti) for stevilka, lastnosti in data.get("parcele", {}).items()}
+        rezervacije = {stevilka:Rezervacija.iz_slovarja(lastnosti) for stevilka, lastnosti in data.get("rezervacije", {}).items()}
         
-        return Model(dat, parcele, rezervacije)        
+        return Model(datoteka, parcele, rezervacije)        
     
     def shrani(self):
         data = {}
-        data["parcele"] = {k:v.v_slovar() for k,v in self.parcele.items()}
-        data["rezervacije"] = {k:v.v_slovar() for k,v in self.rezervacije.items()}
+        data["parcele"] = {stevilka:lastnosti.v_slovar() for stevilka, lastnosti in self.parcele.items()}
+        data["rezervacije"] = {stevilka:lastnosti.v_slovar() for stevilka, lastnosti in self.rezervacije.items()}
         
-        with open(self.datoteka,"w",encoding='utf-8') as write_file:
-            json.dump(data,write_file,indent=4, ensure_ascii=False)
+        with open(self.datoteka,"w", encoding='utf-8') as write_file:
+            json.dump(data, write_file, indent=4, ensure_ascii=False)
